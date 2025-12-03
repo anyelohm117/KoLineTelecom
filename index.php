1fabd7bf5a013080a9f39efaad94559c27ad15ea
anyelohm117 <anyelohermar20@gmail.com>
2025-12-03 00:51:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
     <!-- Logo y títulos -->
     <div class="text-center mb-8">
         <img src="imagenes/logo.png" alt="Logo KoLine Telecom" 
-             class="w-24 h-24 mx-auto mb-4 rounded-full shadow-neon" />
+             class="w-35 h-35 mx-auto mb-4 rounded-full shadow-neon" />
 
         <h1 class="text-3xl font-bold text-koline-primary tracking-wider shadow-neon/50">
             KoLine Telecom
